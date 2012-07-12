e3524f2e470a2ee66bd837a743b44c347a7465cd
Miriam <kihitara@gmail.com>
2012-07-12 12:08:16 +1200

    Fixed green link colours, now gold.

--- README.txt
+++ README.txt
@@ -2,3 +2,11 @@ lynfield2
 =========
 
 Lynfield theme for Moodle 2.1
+
+=========
+Changes:
+=========
+
+12/07/12
+- Lots of links same colour as background
+  fixed now.
--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012022300; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2012071200; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011070100; // Requires this Moodle version
 $plugin->component = 'theme_lynfield2'; // Full name of the plugin (used for diagnostics)
